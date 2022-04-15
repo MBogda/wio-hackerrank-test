3ddcb0caefa60289a4fe4b8d747e40e8609c7ce5
Bogdan Markov <bogdick@yandex.ru>
2022-04-15 17:44:39 +0200

    Get all with date filtering and ordering (but without city filtering) method is created and test is passed.

--- src/main/java/com/hackerrank/weather/controller/WeatherApiRestController.java
+++ src/main/java/com/hackerrank/weather/controller/WeatherApiRestController.java
@@ -3,7 +3,9 @@ package com.hackerrank.weather.controller;
 import com.hackerrank.weather.model.Weather;
 import com.hackerrank.weather.repository.WeatherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -26,10 +28,43 @@ public class WeatherApiRestController {
 
     @GetMapping
     public List<Weather> getWeather(
-            @RequestParam(required = false) Date date,
-            @RequestParam(required = false) String city,
-            @RequestParam(required = false) String sort) {
-        return weatherRepository.findByOrderByIdAsc();
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Nullable Date date,
+            @RequestParam(required = false) @Nullable String city,
+            @RequestParam(required = false) @Nullable String sort) {
+        // todo: proper city filtering
+        if (sort == null) {
+            if (date != null && city != null) {
+                return weatherRepository.findByDateAndCityOrderByIdAsc(date, city);
+            } else if (date != null) {
+                return weatherRepository.findByDateOrderByIdAsc(date);
+            } else if (city != null) {
+                return weatherRepository.findByCityOrderByIdAsc(city);
+            } else {
+                return weatherRepository.findByOrderByIdAsc();
+            }
+        } else if (sort.equals("date")) {
+            if (date != null && city != null) {
+                return weatherRepository.findByDateAndCityOrderByDateAscIdAsc(date, city);
+            } else if (date != null) {
+                return weatherRepository.findByDateOrderByDateAscIdAsc(date);
+            } else if (city != null) {
+                return weatherRepository.findByCityOrderByDateAscIdAsc(city);
+            } else {
+                return weatherRepository.findByOrderByDateAscIdAsc();
+            }
+        } else if (sort.equals("-date")) {
+            if (date != null && city != null) {
+                return weatherRepository.findByDateAndCityOrderByDateDescIdAsc(date, city);
+            } else if (date != null) {
+                return weatherRepository.findByDateOrderByDateDescIdAsc(date);
+            } else if (city != null) {
+                return weatherRepository.findByCityOrderByDateDescIdAsc(city);
+            } else {
+                return weatherRepository.findByOrderByDateDescIdAsc();
+            }
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid `sort` parameter.");
+        }
     }
 
     @GetMapping("/{id}")
--- src/main/java/com/hackerrank/weather/repository/WeatherRepository.java
+++ src/main/java/com/hackerrank/weather/repository/WeatherRepository.java
@@ -3,8 +3,31 @@ package com.hackerrank.weather.repository;
 import com.hackerrank.weather.model.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface WeatherRepository extends JpaRepository<Weather, Integer> {
     List<Weather> findByOrderByIdAsc();
+
+    List<Weather> findByOrderByDateAscIdAsc();
+
+    List<Weather> findByOrderByDateDescIdAsc();
+
+    List<Weather> findByDateOrderByIdAsc(Date date);
+
+    List<Weather> findByDateOrderByDateAscIdAsc(Date date);
+
+    List<Weather> findByDateOrderByDateDescIdAsc(Date date);
+
+    List<Weather> findByCityOrderByIdAsc(String city);
+
+    List<Weather> findByCityOrderByDateAscIdAsc(String city);
+
+    List<Weather> findByCityOrderByDateDescIdAsc(String city);
+
+    List<Weather> findByDateAndCityOrderByIdAsc(Date date, String city);
+
+    List<Weather> findByDateAndCityOrderByDateAscIdAsc(Date date, String city);
+
+    List<Weather> findByDateAndCityOrderByDateDescIdAsc(Date date, String city);
 }
