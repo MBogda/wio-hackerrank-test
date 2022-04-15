c7b1221e97b3304ae1e38fa87bd7594e9f514a23
Bogdan Markov <bogdick@yandex.ru>
2022-04-15 17:15:00 +0200

    Get by id method is created and test is passed.

--- src/main/java/com/hackerrank/weather/controller/WeatherApiRestController.java
+++ src/main/java/com/hackerrank/weather/controller/WeatherApiRestController.java
@@ -5,9 +5,11 @@ import com.hackerrank.weather.repository.WeatherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/weather")
@@ -28,5 +30,15 @@ public class WeatherApiRestController {
             @RequestParam(required = false) String city,
             @RequestParam(required = false) String sort) {
         return weatherRepository.findByOrderByIdAsc();
+    }
+
+    @GetMapping("/{id}")
+    public Weather getWeatherById(@PathVariable Integer id) {
+        Optional<Weather> weather = weatherRepository.findById(id);
+        if (weather.isPresent()) {
+            return weather.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
